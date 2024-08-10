7ce2b1b7a1c0bc3e8abba14989a8b4bd447ce53e
Guilherme Gare <ggare.contato@gmail.com>
2024-08-09 22:53:52 -0300

    tests in the task

--- newScript.js
+++ newScript.js
@@ -4,10 +4,51 @@ const extraHoursButton = document.getElementById("extraHoursButton");
 const extraHoursBox = document.getElementById("extraHoursBox");
 const monthDayBox = document.getElementById("monthDayBox");
 const monthDayButton = document.getElementById("monthDayButton");
+const inputQtFilhos = document.getElementById("inputQtFilhos");
+const inputQtHorasSeg = document.getElementById("inputQtHoras");
+const inputQtHorasFeriado = document.getElementById("inputQtHorasFeriado");
 
-const removeHidden = (yesButton, boxHidden) => {
+const yesExtraHour = document.getElementById("yesExtraHour");
+const noExtraHour = document.getElementById("noExtraHour");
+const hasExtraHourDiv = document.getElementById("hasExtraHourDiv");
+
+const yesFeriado = document.getElementById("yesFeriado");
+const noFeriado = document.getElementById("noFeriado");
+const hasFeriadoDiv = document.getElementById("hasFeriadoDiv");
+
+enterDataButton.addEventListener("click", () => {
+  if (
+    dataEnterBox.style.height === "350px" ||
+    dataEnterBox.style.height === "420px"
+  ) {
+    dataEnterBox.style.height = "70px";
+    dataEnterBox.style.cursor = "pointer";
+  } else if (document.getElementById("hasChildren").hasAttribute("hidden")) {
+    dataEnterBox.style.height = "350px";
+    dataEnterBox.style.cursor = "default";
+  } else if (
+    document.getElementById("hasChildren").hasAttribute("hidden") === false
+  ) {
+    dataEnterBox.style.height = "420px";
+  }
+});
+
+document.getElementById("yes13YO").addEventListener("click", () => {
+  dataEnterBox.style.height = "420px";
+  setTimeout(() => {
+    document.getElementById("hasChildren").removeAttribute("hidden");
+  }, 1100);
+});
+
+document.getElementById("no13YO").addEventListener("click", () => {
+  dataEnterBox.style.height = "350px";
+  document.getElementById("hasChildren").setAttribute("hidden", "");
+});
+
+const removeHidden = (yesButton, boxHidden, input) => {
   document.getElementById(yesButton).addEventListener("click", function () {
     document.getElementById(boxHidden).removeAttribute("hidden");
+    input.focus();
   });
 };
 
@@ -22,22 +63,70 @@ const openBox = (h2Button, box) => {
     if (box.style.height === "490px") {
       box.style.height = "70px";
       box.style.cursor = "pointer";
+    } else {
+      box.style.height = "490px";
+      box.style.cursor = "default";
     }
-
-    box.style.height = "490px";
-    box.style.cursor = "default";
   });
 };
 
-removeHidden("yes13YO", "hasChildren");
-setHidden("no13YO", "hasChildren");
+extraHoursButton.addEventListener("click", () => {
+  if (
+    extraHoursBox.style.height === "70px" ||
+    hasExtraHourDiv.hasAttribute("hidden") === false
+  ) {
+    extraHoursBox.style.height = "350px";
+  } else if (
+    extraHoursBox.style.height === "350px" &&
+    hasExtraHourDiv.hasAttribute("hidden") &&
+    hasFeriadoDiv.hasAttribute("hidden")
+  ) {
+    extraHoursBox.style.height = "70px";
+  } else if (
+    hasExtraHourDiv.hasAttribute("hidden") === false &&
+    hasFeriadoDiv.hasAttribute("hidden") === false &&
+    extraHoursBox.style.height === "500px"
+  ) {
+    extraHoursBox.style.height = "70px";
+  }
+});
 
-removeHidden("yesExtraHour", "hasExtraHourDiv");
-setHidden("noExtraHour", "hasExtraHourDiv");
+yesExtraHour.addEventListener("click", () => {
+  extraHoursBox.style.height = "420px";
 
-removeHidden("yesFeriado", "hasFeriadoDiv");
-setHidden("noFeriado", "hasFeriadoDiv");
+  setTimeout(() => {
+    hasExtraHourDiv.removeAttribute("hidden");
+  }, 1400);
+});
+
+noExtraHour.addEventListener("click", () => {
+  hasExtraHourDiv.setAttribute("hidden", "");
+  if (hasFeriadoDiv.hasAttribute("hidden")) {
+    extraHoursBox.style.height = "350px";
+  } else {
+    extraHoursBox.style.height = "500px";
+  }
+});
+
+yesFeriado.addEventListener("click", () => {
+  if (hasExtraHourDiv.hasAttribute("hidden")) {
+    extraHoursBox.style.height = "400px";
+  } else {
+    extraHoursBox.style.height = "500px";
+  }
+
+  setTimeout(() => {
+    hasFeriadoDiv.removeAttribute("hidden");
+  }, 1400);
+});
+
+noFeriado.addEventListener("click", () => {
+  hasFeriadoDiv.setAttribute("hidden", "");
+  if (hasExtraHourDiv.hasAttribute("hidden") === false) {
+    extraHoursBox.style.height = "400px";
+  } else {
+    extraHoursBox.style.height = "350px";
+  }
+});
 
 openBox(monthDayButton, monthDayBox);
-openBox(extraHoursButton, extraHoursBox);
-openBox(enterDataButton, dataEnterBox);
